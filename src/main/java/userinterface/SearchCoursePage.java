bb6ac458e37f6277dd6013ce078889452e7b2752
mateoGom <matemgm@hotmail.com>
2021-03-22 16:09:12 +0100

    correccion archivo gitignore y correccion de paginas que extienden de page object

--- src/main/java/userinterface/SearchCoursePage.java
+++ src/main/java/userinterface/SearchCoursePage.java
@@ -1,10 +1,9 @@
 package userinterface;
 
-import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.screenplay.targets.Target;
 import org.openqa.selenium.By;
 
-public class SearchCoursePage  extends PageObject {
+public class SearchCoursePage  {
     public static final Target BUTTON_UC =Target.the("selecciona la universida choucair").located(By.xpath("//div[@id='certificaciones']//strong"));
     public static final Target INPUT_OCURSE = Target. the("Buscar el curso").located(By.id("coursesearchbox"));
     public static final Target BUTTON_GO=Target.the("Da click para buscar el curso").located(By.xpath("//button[@class='btn btn-secondary']"));
